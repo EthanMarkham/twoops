133fced40a67f3367b1f82473d5458644c2c1a1f
EthanMarkham <markham.ethan@gmail.com>
2021-10-02 22:41:03 -0400

    quick loading screen fix

--- src/client/components/Loading.tsx
+++ src/client/components/Loading.tsx
@@ -1,26 +1,10 @@
 import React from "react";
-import { useSpring, config } from "react-spring";
-import { AnimatedWrapper, Img, PageHolder } from "../styles";
+import {  PageHolder } from "../styles";
 
 const Loading = (props: any) => {
-    const spin = useSpring({
-        from: {
-            rotateZ: 180,
-            opacity: 0.9,
-        },
-        to: {
-            rotateZ: 0,
-            opacity: 1,
-        },
-        loop: true,
-        config: config.molasses,
-    });
-
     return (
         <PageHolder center={true}>
-            <AnimatedWrapper style={spin}>
-                <Img src="/assets/img/bucketsLogo.png" />
-            </AnimatedWrapper>
+         
         </PageHolder>
     );
 };
